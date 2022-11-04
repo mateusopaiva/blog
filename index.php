2c31a5f0ba5e349fd81c45275691d45343fdbf99
Mateus Paiva <mateusopaiva@gmail.com>
2022-11-04 10:28:01 -0300

    Obtendo os artigos do banco de dados

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
-    include 'Artigo.php'; 
-    $artigo = new Artigo();
+
+    require 'config.php';
+
+    include 'src/Artigo.php'; 
+    $artigo = new Artigo($mysql);
     $artigos = $artigo->exibirTodos();
 ?>
 
@@ -16,16 +19,16 @@
     <body>
         <div id="container">
             <h1>Meu Blog</h1>
-            <?php foreach($artigos as $artigo): ?>
+            <?php foreach($artigos as $artigo) : ?>
             <h2>
-                <a href="<?php echo $artigo['link'] ?>">
-                    <?php echo $artigo['titulo'] ?>
+                <a href="artigo.php?id=<?php echo $artigo['id']; ?>">
+                    <?php echo $artigo['titulo']; ?>
                 </a>
             </h2>
             <p>
-                <?php echo $artigo['conteudo'] ?>
+                <?php echo $artigo['conteudo']; ?>
             </p>
-            <?php endforeach ?>
+            <?php endforeach; ?>
         </div>
     </body>
 
